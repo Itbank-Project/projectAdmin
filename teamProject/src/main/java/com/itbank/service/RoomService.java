2c8ff7b3ea920c90a5838fc42899155eec70f4fb
gkdld1004 <74804511+gkdld1004@users.noreply.github.com>
2021-03-26 09:41:18 +0900

    change

--- teamProject/src/main/java/com/itbank/service/RoomService.java
+++ teamProject/src/main/java/com/itbank/service/RoomService.java
@@ -1,5 +1,7 @@
 package com.itbank.service;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -14,6 +16,11 @@ public class RoomService {
 
 	public int insertRoom(RoomDTO dto) {
 		return dao.insertRoom(dto);
+	}
+
+
+	public List<RoomDTO> getList() {
+		return dao.getList();
 	}
 
 }
