3bf1b38d04303dfe5e47dda947056f3fe9492b38
yeeun-jo <62788493+yeeun-jo@users.noreply.github.com>
2021-03-27 21:19:31 +0900

    매개변수 수정

--- teamProject/src/main/java/com/itbank/calendar/CalendarDAO.java
+++ teamProject/src/main/java/com/itbank/calendar/CalendarDAO.java
@@ -1,10 +1,13 @@
 package com.itbank.calendar;
 
+import java.util.HashMap;
+import java.util.List;
 
 public interface CalendarDAO {
 
 	// 객실타입에 따른 캘린더 
-	CalendarDTO getList(String ro_pk);
+//	CalendarDTO getList(String ro_pk);
+	List<CalendarDTO> getList(HashMap<String, Object> map);
 
 	// 캘린더추가
 	int insertCalendar(CalendarDTO dto);
