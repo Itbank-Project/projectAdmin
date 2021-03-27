f90cd297549bed097ec19719d9e9d52eda4e6f23
yeeun-jo <62788493+yeeun-jo@users.noreply.github.com>
2021-03-27 21:18:38 +0900

    객실현황 수정

--- teamProject/src/main/java/com/itbank/controller/MainController.java
+++ teamProject/src/main/java/com/itbank/controller/MainController.java
@@ -1,6 +1,7 @@
 package com.itbank.controller;
 
 import java.io.IOException;
+import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.HashMap;
 import java.util.List;
@@ -131,39 +132,50 @@ public class MainController {
 		
 		ModelAndView mav = new ModelAndView("roomStatus");
 		
-//		List<CalendarDTO> calendar = cs.getList();
+		// 오늘 날짜 가져와서 map에 넣기
+		SimpleDateFormat format = new SimpleDateFormat("yyyy-MM");
+		Date time = new Date();
+				
+		String nowDate = format.format(time);
+				
+		HashMap<String, Object> map = new HashMap<String, Object>();
+		map.put("ho_name", ho_name);
+		map.put("nowDate", nowDate);
+		
+		
+		List<CalendarDTO> calendar = cs.getList(map);
 		List<RoomDTO> roomList = rs.getList(ho_name);
 		
 		int roomCount = roomList.size();
 		
-//		mav.addObject("calendar", calendar);
+		mav.addObject("dto", calendar);
 		mav.addObject("roomList", roomList);
 		mav.addObject("roomCount", roomCount);
 		
 		return mav;
 	}
 	
-	@PostMapping("roomStatus")
-	public ModelAndView roomStatus(CalendarDTO dto) {
-		ModelAndView mav = new ModelAndView("roomStatus");
-		
-		System.out.println(dto.getCalendar_count());
-		System.out.println(dto.getCalendar_ro_pk());
-		System.out.println(dto.getCalendar_pk());
-		System.out.println(dto.getCalendar_price());
-		System.out.println(dto.getCalendar_date());
-		
-		String ro_pk = dto.getCalendar_ro_pk();
-		
-		dto.setCalendar_pk(dto.getCalendar_date() + "(" + ro_pk + ")");
-		System.out.println(dto.getCalendar_pk());
-		int row = cs.insertCal(dto);
-		if(row == 1 ) {
-			CalendarDTO calDTO = cs.getList(ro_pk);
-			mav.addObject("calDTO", calDTO);
-		}
-		return mav;
-	}
+//	@PostMapping("roomStatus")
+//	public ModelAndView roomStatus(CalendarDTO dto) {
+//		ModelAndView mav = new ModelAndView("roomStatus");
+//		
+//		System.out.println(dto.getCalendar_count());
+//		System.out.println(dto.getCalendar_ro_pk());
+//		System.out.println(dto.getCalendar_pk());
+//		System.out.println(dto.getCalendar_price());
+//		System.out.println(dto.getCalendar_date());
+//		
+//		String ro_pk = dto.getCalendar_ro_pk();
+//		
+//		dto.setCalendar_pk(dto.getCalendar_date() + "(" + ro_pk + ")");
+//		System.out.println(dto.getCalendar_pk());
+//		int row = cs.insertCal(dto);
+//		if(row == 1 ) {
+////			CalendarDTO calDTO = cs.getList(ro_pk);
+////			mav.addObject("calDTO", calDTO);
+//		}
+//		return mav;
+//	}
 	
 	
 	
