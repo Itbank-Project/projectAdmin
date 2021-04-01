f4189d6ac7cf28867e56c567b0abdae4d5dd5605
yeeun-jo <62788493+yeeun-jo@users.noreply.github.com>
2021-04-01 09:27:13 +0900

    Update CalendarAjaxController.java

--- teamProject/src/main/java/com/itbank/controller/CalendarAjaxController.java
+++ teamProject/src/main/java/com/itbank/controller/CalendarAjaxController.java
@@ -1,7 +1,10 @@
 package com.itbank.controller;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -13,21 +16,16 @@ public class CalendarAjaxController {
 
 	@Autowired private CalendarService cs;
 
-	@PostMapping(value = "/roomStatus", consumes = "application/json;charset=utf8")
-	public int roomStatus(@RequestBody CalendarDTO dto) {
-//		ModelAndView mav = new ModelAndView("roomStatus");
-		System.out.println("??");
-		System.out.println(dto.getCalendar_count());
-		System.out.println(dto.getCalendar_ro_pk());
-		System.out.println(dto.getCalendar_pk());
-		System.out.println(dto.getCalendar_price());
-		System.out.println(dto.getCalendar_date());
+	// 객실정보 수정하기
+	@PutMapping(value = "/roomStatus", consumes = "application/json;charset=utf8")
+	public int roomStatus(@RequestBody CalendarDTO dto) throws ParseException {
 		
-		String ro_pk = dto.getCalendar_ro_pk();
+		SimpleDateFormat sdf = new SimpleDateFormat("yy/MM/dd"); 
+		String date = sdf.format(dto.getCalendar_date());
 		
-		dto.setCalendar_pk(dto.getCalendar_date() + "(" + ro_pk + ")");
-		System.out.println(dto.getCalendar_pk());
-		int row = cs.insertCal(dto);
+		dto.setCalendar_pk(date + "-" + dto.getCalendar_pk());
+		int row = cs.updateCal(dto);
+		
 		return row;
 	}
 	
